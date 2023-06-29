42951b2cb71a8357333cf9321cad4c54cd8eaeb4
Mornieur <mariafernanda-coro@hotmail.com>
2023-06-29 15:44:06 -0300

    style: styling view component

---
+++ src/components/ViewComponent/styles.ts
@@ -0,0 +1,146 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  max-width: 1150px;
+  width: 100%;
+  height: 100%;
+  height: 65px;
+  max-height: 300px;
+
+  padding: 1rem;
+  justify-content: center;
+  gap: 1rem;
+
+  @media (max-width: 400px) {
+    gap: 0.5rem;
+  }
+`;
+
+export const Button = styled.button<{ active: boolean }>`
+  padding: 0.5rem 1rem;
+  border-radius: 1.2rem;
+  text-decoration: none;
+  border: 2px solid grey;
+  background-color: #fff;
+  font-size: 1.2rem;
+  height: 3rem;
+  max-width: 300px;
+  font-size: 1rem;
+  max-height: 50px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+
+  ${({ active }) =>
+    active &&
+    css`
+      background-color: var(--leadster-color);
+      color: #fff;
+      border: none;
+    `}
+
+  @media (max-width: 1060px) {
+    font-size: 0.7rem;
+  }
+
+  @media (max-width: 400px) {
+    padding: 0.5rem;
+  }
+
+  &:hover {
+    border-color: var(--leadster-color);
+    color: var(--leadster-color);
+
+    ${({ active }) =>
+      active &&
+      css`
+        color: #fff;
+      `}
+  }
+`;
+
+export const OrderList = styled.div`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  text-align: center;
+  max-width: 450px;
+  height: 50px;
+  width: 100%;
+
+  @media (max-width: 900px) {
+    display: none;
+  }
+
+  p {
+    padding: 1rem 0.5rem;
+    font-weight: 600;
+
+    @media (max-width: 900px) {
+      display: none;
+    }
+  }
+
+  .menu {
+    max-width: 300px;
+    width: 100%;
+  }
+  button {
+    border: none;
+    border-radius: 10px;
+    padding: 10px 20px;
+    font-size: 18px;
+    font-weight: 700;
+    cursor: pointer;
+    width: 100%;
+    text-align: left;
+    margin-bottom: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @media (max-width: 800px) {
+      font-size: 0.7rem;
+    }
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    z-index: 999;
+    background-color: #fff;
+    width: 100%;
+
+    @media (max-width: 880px) {
+      width: 100px;
+      position: relative;
+    }
+  }
+  li {
+    display: block;
+    width: 100%;
+    cursor: pointer;
+    border-radius: 0.7rem;
+
+    :hover {
+      background-color: #dadada;
+    }
+  }
+
+  ul,
+  li {
+    list-style: none;
+    margin: 0;
+    padding: 10px;
+  }
+`;
