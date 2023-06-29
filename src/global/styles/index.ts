cbdc4e88fd2d80003fba4d1d3fd4ae5a59724546
Mornieur <mariafernanda-coro@hotmail.com>
2023-06-29 15:44:27 -0300

    style: creating a global style

---
+++ src/global/styles/index.ts
@@ -0,0 +1,94 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Plus Jakarta Sans Variable';
+    font-weight: 100 800;
+    font-style: normal;
+    font-display: swap;
+    src: url('/fonts/PlusJakartaSans-VariableFont_wght.ttf') format('truetype-variable'), 
+        url('/fonts/static/PlusJakartaSans-ExtraLight.ttf') format('truetype'), 
+        url('/fonts/static/PlusJakartaSans-Light.ttf') format('truetype'), 
+        url('/fonts/static/PlusJakartaSans-Bold.ttf') format('truetype'); 
+  }
+  * {
+    padding:0;
+    margin:0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+    text-decoration: none;
+  }
+
+  img {
+    image-rendering: auto;
+  }
+
+  body {
+    width:100%;
+    height:100%;
+    scrollbar-width: thin;
+  }
+
+  html {
+    @media (min-width: 1600px){
+      font-size: 18px;
+    }
+    @media (min-width:600px){
+      font-size:16px;
+    }
+    @media (max-height:720px){
+      font-size:14px;
+    }
+    @media (max-width:480px){
+      font-size:14px;
+    }
+  }
+
+  li, ul {
+    list-style: none;
+  }
+
+  button {
+    cursor:pointer;
+  }
+
+
+  scroll-behavior: smooth;
+  overflow-x: hidden;
+
+  :root {
+
+
+    // FONTS
+    --montserrat: 'Montserrat', sans-serif;
+    --titillium: 'Titillium Web', sans-serif;
+
+    // FONTS WEIGHT 
+    --light: 300;
+    --regular: 400;
+    --medium: 500;
+    --semibold: 600;
+    --bold: 700;
+
+    // FONTS
+    --montserrat: 'Montserrat', sans-serif;
+    --titillium: 'Titillium Web', sans-serif;
+
+    // FONTS WEIGHT 
+    --light: 300;
+    --regular: 400;
+    --medium: 500;
+    --semibold: 600;
+    --bold: 700;
+
+    // COLORS
+
+    --white-color: #ffffff;
+    --primary-color: #5C5D73;
+    --secondary-color: #ADB0D9;
+    --delete-color: #C70000;
+    --saved-color: #6CA468;
+    --edit-color: #009FFFE8;
+    --leadster-color: #2c83fb;
+  }
+`;
