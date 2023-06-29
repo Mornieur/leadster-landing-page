387fb08e252ae95e552942289969ade6cfbcfb80
Mornieur <mariafernanda-coro@hotmail.com>
2023-06-29 15:19:25 -0300

    build: add storybook settings

---
+++ .storybook/preview.tsx
@@ -0,0 +1,152 @@
+import { GlobalStyle } from '../src/global/styles';
+import * as NextImage from 'next/image';
+import { setupWorker, rest } from 'msw';
+import React, { Suspense } from 'react';
+import type { Preview } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme, customViewports } from './themes';
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary,
+  Controls,
+  Stories,
+} from '@storybook/blocks';
+import { initialize } from 'msw-storybook-addon';
+import { themes } from '@storybook/theming';
+
+initialize({
+  onUnhandledRequest: 'bypass',
+});
+
+if (typeof global.process === 'undefined') {
+  const worker = setupWorker(
+    rest.get('http://localhost:3000/api/hello', (req, res, ctx) => {
+      return res(ctx.json({ name: 'John Doh' }));
+    })
+  );
+  worker.start();
+}
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+const preview: Preview = {
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <Controls />
+          <Stories />
+        </>
+      ),
+      theme: themes.dark,
+    },
+    msw: {
+      handlers: {
+        auth: [
+          rest.get('/essay', (req, res, ctx) => {
+            return res(
+              ctx.json({
+                success: true,
+              })
+            );
+          }),
+          rest.get('/logout', (req, res, ctx) => {
+            return res(
+              ctx.json({
+                success: true,
+              })
+            );
+          }),
+        ],
+      },
+    },
+    storySort: {
+      order: ['Examples', 'Docs', 'Demo'],
+    },
+    viewport: {
+      viewport: {
+        viewports: { ...INITIAL_VIEWPORTS, ...customViewports },
+        defaultViewport: 'iPad',
+      },
+      defaultViewport: 'someDefault',
+    },
+
+    actions: { argTypesRegex: '^on.*' },
+    options: {
+      storySort: {
+        order: ['Intro', 'Components'],
+        method: 'alphabetical',
+        locales: 'pt-BR',
+      },
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+
+  decorators: [
+    (Story, context) => {
+      const theme = context.globals.theme === '' ? darkTheme : lightTheme;
+      const { locale } = context.globals;
+
+      return (
+        <Suspense fallback={<div>loading translations...</div>}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Story />
+          </ThemeProvider>
+        </Suspense>
+      );
+    },
+  ],
+};
+
+export default preview;
+
+export const InputWithoutAutofill = () => <input type="text" />;
+
+InputWithoutAutofill.parameters = {
+  a11y: {
+    disable: true,
+    config: {
+      rules: [
+        {
+          id: 'autocomplete-valid',
+          selector: '*:not([autocomplete="nope"])',
+        },
+        {
+          id: 'landmark-complementary-is-top-level',
+          reviewOnFail: true,
+        },
+      ],
+    },
+  },
+};
+
+export const Inaccessible = () => (
+  <button style={{ backgroundColor: 'red', color: 'darkRed' }}>
+    Inaccessible button
+  </button>
+);
+Inaccessible.parameters = {
+  a11y: {
+    config: {
+      rules: [{ id: 'color-contrast', enabled: false }],
+    },
+  },
+};
