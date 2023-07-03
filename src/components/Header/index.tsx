bfa1c1f3c45258dd9e3a95e18554440a1b9f2b2e
Mornieur <mariafernanda-coro@hotmail.com>
2023-07-03 14:59:41 -0300

    feat: creating header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,29 @@ import Image from 'next/image';
 import * as S from './styles';
 import logo from '../../assets/images/logo.png';
 import assetHeader from '../../assets/images/asset-header.png';
+import { FC } from 'react';
 
-export const Header = () => {
+interface HeaderProps {
+  minText?: string;
+  minTitle?: string;
+  maxTitle?: string;
+}
+
+export const Header: FC<HeaderProps> = ({
+  minText = 'WEBNARS EXCLUSIVOS',
+  minTitle = 'Menos Conversinha,',
+  maxTitle = 'Mais Conversão',
+}) => {
   return (
     <S.Container>
       <article>
         <Image src={logo} width={200} height={100} alt="logo" />
       </article>
       <S.TitleComponent>
-        <S.MinText>WEBNARS EXCLUSIVOS</S.MinText>
-        <S.MinTitle>Menos Conversinha,</S.MinTitle>
+        <S.MinText>{minText}</S.MinText>
+        <S.MinTitle>{minTitle}</S.MinTitle>
         <S.MaxTitle>
-          Mais Conversão
+          {maxTitle}
           <Image
             className="minLogo"
             src={assetHeader}
