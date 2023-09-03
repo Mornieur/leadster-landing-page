a80807f0e38b4ea9a995082cfe6f0a4f50581dea
Mornieur <mariafernanda-coro@hotmail.com>
2023-09-03 14:58:09 -0300

    perf: add filter components

--- src/components/VideoComponent/index.tsx
+++ src/components/VideoComponent/index.tsx
@@ -1,65 +1,109 @@
 import * as S from './styles';
-import ThumbnailImage from '../../assets/images/thumbnail.png';
 import videos from './data.json';
 import { useEffect, useState } from 'react';
 import { ModalView } from './ModalView';
 import { BsFillPlayFill } from 'react-icons/bs';
 import { motion } from 'framer-motion';
+import { useVideoStore } from '@/store';
+import { VideoCategory } from '@/pages';
+
+interface Video {
+  id: number;
+  title: string;
+  alt: string;
+  link: string;
+  image: string;
+  category: string;
+  createdAt: string;
+}
 
 export const VideoComponent = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  useEffect(() => {
-    document.body.style.overflow = isModalOpen ? 'hidden' : 'auto';
-  }, [isModalOpen]);
+  const [modalData, setModalData] = useState<{
+    link: string;
+    description: string;
+  } | null>(null);
+  const { orderBy, category } = useVideoStore();
 
-  const openModal = () => {
-    setIsModalOpen(true);
+  useEffect(() => {
+    document.body.style.overflow = modalData ? 'hidden' : 'auto';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [modalData]);
+
+  const openModal = (videoLink: string, videoDescription: string) => {
+    setModalData({ link: videoLink, description: videoDescription });
   };
 
   const closeModal = () => {
-    setIsModalOpen(false);
+    setModalData(null);
   };
+
+  const sortListByDate = () => {
+    return videos.sort((a, b) => {
+      const dateA = new Date(a.createdAt).getTime();
+      const dateB = new Date(b.createdAt).getTime();
+      const crescent = orderBy === 'Mais Recente';
+
+      return crescent ? dateA - dateB : dateB - dateA;
+    });
+  };
+
+  const filterVideosByCategory = (videos: Video[], category: VideoCategory) => {
+    if (category === null) {
+      return videos;
+    }
+    return videos.filter((video) => video.category === category);
+  };
+
+  const sortedAndFilteredVideos = sortListByDate();
+  const filteredVideos = filterVideosByCategory(
+    sortedAndFilteredVideos,
+    category
+  );
 
   return (
     <>
-      {isModalOpen && (
-        <ModalView setIsOpen={() => closeModal()} isOpen={isModalOpen} />
+      {modalData && (
+        <ModalView
+          setIsOpen={closeModal}
+          isOpen={!!modalData}
+          videoData={modalData}
+        />
       )}
-      <S.Container isOpen={isModalOpen}>
-        {videos.map((video, index) => (
-          <>
-            <motion.div
-              className="videoGroup"
-              initial={{ opacity: 0, scale: 0.5 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{
-                duration: 0.8,
-                delay: 0.5,
-                ease: [0, 0.71, 0.2, 1.01],
-              }}
+      <S.Container isOpen={!!modalData}>
+        {filteredVideos.map((video, index) => (
+          <motion.div
+            key={index}
+            className="videoGroup"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.5,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+          >
+            <S.VideoCard
+              key={index}
+              onClick={() => openModal(video.link, video.title)}
+              data-testid={`video-card-${index}`}
             >
-              <S.VideoCard
-                key={video.id}
-                onClick={openModal}
-                data-testid={`video-card-${index}`}
-              >
-                <S.PlayIcon>
-                  <BsFillPlayFill size={'8rem'} color="#fff" />
-                </S.PlayIcon>
-                <S.VideoPreview>
-                  <S.ThumbnailImage
-                    src={ThumbnailImage.src}
-                    alt={video.alt}
-                    height={300}
-                    data-testid={`thumbnail-${index}`}
-                  />
-                  <S.Description data-testid={`video-title-${index}`}>
-                    {video.title}
-                  </S.Description>
-                </S.VideoPreview>
-              </S.VideoCard>
-            </motion.div>
-          </>
+              <S.PlayIcon>
+                <BsFillPlayFill size={'8rem'} color="#fff" />
+              </S.PlayIcon>
+              <S.VideoPreview>
+                <S.ThumbnailImage
+                  src={video.image}
+                  alt={video.alt}
+                  data-testid={`thumbnail-${index}`}
+                />
+                <S.Description data-testid={`video-title-${index}`}>
+                  {video.title}
+                </S.Description>
+              </S.VideoPreview>
+            </S.VideoCard>
+          </motion.div>
         ))}
       </S.Container>
     </>
